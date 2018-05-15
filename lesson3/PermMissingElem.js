f4c259b60ee3e31719acbe76556d97eceb5ffa6b
Jancleidsson Soares <jss@cesar.org.br>
2018-05-15 11:31:19 -0300

    Added PermMissingElem response

---
+++ lesson3/PermMissingElem.js
@@ -0,0 +1,12 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+// Detected time complexity:O(N) or O(N * log(N))
+
+function solution(A) {
+    let currentCalc = 0;
+    for (let index = 0; index < A.length; index++) {
+          currentCalc += A[index];
+     }
+     return ((A.length + 1) * ((A.length + 1) + 1) / 2) - currentCalc;
+ }
